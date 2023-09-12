66af2774107851d0a6a7d1f9dfbe4279caa63552
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-09-12 22:08:09 +0300

    mongoose connect

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-const app = require('./app')
+const mongoose = require('mongoose');
 
-app.listen(3000, () => {
-  console.log("Server running. Use our API on port: 3000")
+const app = require('./app');
+
+const CONTACTS_HOST = "mongodb+srv://Melnyk:<WalkingNewbie2024>@cluster1.a15gywb.mongodb.net/db-contacts?retryWrites=true&w=majority"
+
+mongoose.set('strictQuery', true);
+
+mongoose.connect(CONTACTS_HOST).then(
+  ()=>{
+    app.listen(3000, () => { console.log("Server running. Use our API on port: 3000")})
+}).catch( error =>{
+  console.log(error.message);
+  process.exit(1);
 })
