66daf9ba5973750272d454fc5f4d70738e501dcd
ColonelMelnyk <119069975+ColonelMelnyk@users.noreply.github.com>
2023-10-03 19:28:23 +0300

    pt3

--- error_handler/emailSender.js
+++ error_handler/emailSender.js
@@ -1,14 +1,26 @@
-const sgMail = require("@sendgrid/mail");
+const nodemailer = require('nodemailer');
 require("dotenv").config();
+const {META_PASSWORD} = process.env;
 
-const {API_KEY} = process.env;
+const nodemailerConfig = {
+    host: "smtp.meta.ua",
+    port: 465,
+    secure: true,
+    auth: {
+        user: "mr.melnyk2024@meta.ua",
+        pass: META_PASSWORD
+    }
+};
 
-sgMail.setApiKey(API_KEY);
+const transport = nodemailer.createTransport(nodemailerConfig);
 
-const emailSender = async (data) => {
-    const email = {...data, from: "maratkiller16@gmail.com"};
-    await sgMail.send(email);
-    return true;
-}
+const emailSender = data => {
+    const email = { 
+        to: "kinedav149@huvacliq.com",
+        subject: "Test email",
+        html: "<p><strong>Test email</strong> from localhost:3000</p>", 
+        from: "mr.melnyk2024@meta.ua" };
+    return transport.sendMail(email);
+};
 
 module.exports = emailSender;
